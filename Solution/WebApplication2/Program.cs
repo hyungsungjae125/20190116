6d3eecbd82729b52c4a3b7c9dfadc62285858915
형성재 <tldwo125@naver.com>
2019-01-16 13:18:25 +0900

    80포트 추가

--- Solution/WebApplication2/Program.cs
+++ Solution/WebApplication2/Program.cs
@@ -19,6 +19,7 @@ namespace WebApplication2
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:80");
     }
 }
